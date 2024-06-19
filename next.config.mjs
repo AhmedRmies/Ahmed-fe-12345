79e160ad60724d50f326760c71cf0ddabbc2f6c0
Ahmed Masada <121398355+AhmedRmies@users.noreply.github.com>
2024-06-19 17:58:00 +0800

    config updated

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@ const nextConfig = {
             {
                 protocol:'https',
                 hostname:'image.tmdb.org',
-                pathname:'**'
+                pathname:'**',
+                output:'export'
             },
         ],
     },
